f11a315d5056b11a41e19819284bb0e7639732a3
vigneshmohan2002 <85409344+vigneshmohan2002@users.noreply.github.com>
2023-02-21 19:31:34 +0000

    session authentication

--- src/trashmain/settings.py
+++ src/trashmain/settings.py
@@ -128,3 +128,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'trashusers.User'
+
+REST_FRAMEWORK = {
+
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.SessionAuthentication",
+    ),
+}
