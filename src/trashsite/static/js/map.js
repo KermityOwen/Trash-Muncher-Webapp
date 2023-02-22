ff77350141e5d4894fe68c92174a377e7c13ef98
TerraTree <wliversidge88@gmail.com>
2023-02-22 16:34:49 +0000

    basic form upload to server for adding points

--- src/trashsite/static/js/map.js
+++ src/trashsite/static/js/map.js
@@ -20,19 +20,49 @@ map = new google.maps.Map(document.getElementById("map"), {
 }
 
 //will be called on a click event with the gmaps api, passes in longitude, latitude
-function placeMonster(){
+function placeMonster(){ //need to add parameters once working
   //creates a form to add a monster to the map
   if(document.getElementById("form")){
 
   }
   else{
-  alert("cool monster button");
-  const form = document.createElement("div");
-  form.id="form";
-  const button = document.createElement("button");
-  button.textContent="button";
-  form.append(button);
-  document.body.append(form);
+    const form = document.createElement("form");
+    form.action="/map";
+    form.id="form";
+    form.method="post";
+
+    const label = document.createElement("label");
+    label.for="lat";
+    label.textContent="Latitude";
+    form.append(label);
+    const lat = document.createElement("input");
+    lat.name="lat";
+    lat.id = "lat";
+    lat.value=50.73646948193597;
+    lat.readOnly = true;
+    form.append(lat);
+    form.append(document.createElement("br"));
+
+    const label2 = document.createElement("label");
+    label2.for="lng";
+    label2.textContent="Longitude";
+    form.append(label2);
+    const lng = document.createElement("input");
+    lng.name="lat";
+    lng.value=-3.5317420013942633;
+    lng.readOnly = true;
+    form.append(lng);
+    form.append(document.createElement("br"));
+
+    const button = document.createElement("input");
+    button.type="submit";
+    button.value="Submit";
+    form.append(button);
+    document.body.append(form);
   }
+}
+
+function ajax(){
+  alert("this will do ajax shit soon");
 }
 window.initMap = initMap;
