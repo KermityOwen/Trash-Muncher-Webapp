a9439e89ebc95f29bee85c3b80fdd8b5626a75d4
Ellie Kelemen <37041249+whoisEllie@users.noreply.github.com>
2023-02-23 15:00:53 +0000

    working form interrupt & basic styling

--- src/trashsite/views.py
+++ src/trashsite/views.py
@@ -6,3 +6,9 @@ def index(request):
 
 def map(request):
     return render(request, 'trashsite/map.html')
+
+def imageapprove(request):
+    return render(request, 'trashsite/imageapprove.html')
+
+def mapselect(request):
+    return render(request, 'trashsite/mapselect.html')
