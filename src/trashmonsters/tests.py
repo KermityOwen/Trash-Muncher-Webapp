2d3249f5d74e9e944a3b4fca26b69f5256265c8d
vigneshmohan2002 <85409344+vigneshmohan2002@users.noreply.github.com>
2023-03-06 13:19:02 +0000

    pipfile update

--- src/trashmonsters/tests.py
+++ src/trashmonsters/tests.py
@@ -1,3 +1,67 @@
-from django.test import TestCase
+from rest_framework.test import (
+    APITestCase,
+    APIClient,
+)
+from rest_framework import status
+from .models import Player, GameKeeper, User, Team
+from django.contrib.auth.models import Group
 
-# Create your tests here.
+
+class UserViewsetTest(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create(username="first_user", first_name="first", last_name="user",password="secure_password_rock")
+        self.player = Player.objects.create(
+            user=self.user, team=Team.objects.get_or_create(id=1, name='Red')[0]
+        )
+        self.client = APIClient()
+        self.url = "/api"
+
+    def test_create_player(self):
+        self.client.force_authenticate()
+        response = self.client.post(
+            self.url + "/users/player-register/",
+            {
+                "user": 
+                    {
+                        "username": "test_user",
+                        "first_name": "Test",
+                        "last_name": "User",
+                        "password": "secure_password"
+                    },
+                "team": {
+                    "name": 'Red'
+                }
+            },
+            format='json',
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_gamekeeper(self):
+        self.client.force_authenticate()
+        response = self.client.post(
+            self.url + "/users/gamekeeper-register/",
+            {
+                "user": 
+                    {
+                        "username": "test_user",
+                        "first_name": "Test",
+                        "last_name": "User",
+                        "password": "secure_password"
+                    }
+            },
+            format='json',
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_user(self):
+        self.client.force_authenticate(self.user)
+        response = self.client.get(self.url + "/users/me/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["user"]["username"], self.user.username)
+
+    def test_retreive_user(self):
+        self.client.force_authenticate(self.user)
+        response = self.client.get(self.url + "/users/1/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["id"], self.user.id)
+        self.assertEqual(response.data["username"], self.user.username)
