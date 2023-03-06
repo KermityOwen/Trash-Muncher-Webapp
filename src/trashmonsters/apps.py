3447d0acff0519fbe26685b5909a679c058ba043
vigneshmohan2002 <85409344+vigneshmohan2002@users.noreply.github.com>
2023-03-06 13:29:52 +0000

    commenting out ready function

--- src/trashmonsters/apps.py
+++ src/trashmonsters/apps.py
@@ -5,12 +5,12 @@ class TrashmonstersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'trashmonsters'
 
-    def ready(self):
-        from . import jobs, viewset
+    # def ready(self):
+    #     from . import jobs, viewset
 
-        if os.environ.get('RUN_MAIN', None) != 'true':
-            # On server start, code below will be ran.
-            print("server up")
-            # viewset.restart_testing_db()
-            viewset.calculate_cached_leader()
-            jobs.start_scheduler()
+    #     if os.environ.get('RUN_MAIN', None) != 'true':
+    #         # On server start, code below will be ran.
+    #         print("server up")
+    #         # viewset.restart_testing_db()
+    #         viewset.calculate_cached_leader()
+    #         jobs.start_scheduler()
