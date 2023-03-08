9bec1b33e9d55c24714cb33a40da077bdc51e29b
Owen Lee <owentheturkey@gmail.com>
2023-03-08 22:14:10 +0800

    fix

--- src/trashmonsters/apps.py
+++ src/trashmonsters/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
-import os
+import os, sys
 
 class TrashmonstersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -11,6 +11,8 @@ class TrashmonstersConfig(AppConfig):
         if os.environ.get('RUN_MAIN', None) != 'true':
             # On server start, code below will be ran.
             print("server up")
+            if 'runserver' not in sys.argv:
+                return True
             try:
                 viewset.restart_testing_db()
             except:
