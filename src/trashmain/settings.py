ffa70d6eea77f85417e00e7f77210d2a58fa5718
vigneshmohan2002 <85409344+vigneshmohan2002@users.noreply.github.com>
2023-03-09 11:25:13 +0000

    added rate limiting

--- src/trashmain/settings.py
+++ src/trashmain/settings.py
@@ -159,6 +159,14 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '1000/day',
+        'user': '1000/day'
+    }
 }
 
 SIMPLE_JWT = {
