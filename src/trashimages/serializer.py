66bdbbc09586475562e9cb1885ce527f69980d57
Malik Besta <malik770@hotmail.co.uk>
2023-03-15 22:34:23 +0000

    Base64 Image Handling
    Added functionality to allow base64 images to be submitted

--- src/trashimages/serializer.py
+++ src/trashimages/serializer.py
@@ -1,13 +1,38 @@
 from rest_framework import serializers
+from django.core.files.base import ContentFile
+import base64
 from .models import Images
+from trashusers.models import Team
+from trashmonsters.models import TrashMonsters
+from uuid import uuid4
+
+
+def base64_to_img(data):
+        format, img_str = data.split(';base64,')
+        ext = format.split('/')
+        return ContentFile(base64.b64decode(img_str + "=="), name=uuid4().hex + "." + ext[1])
+
 
 class ImageSerializer(serializers.ModelSerializer):
     """ 
     Used to get specific attributes from the database in JSON format
     """
+
     class Meta:
         """ 
         Specifies which model the fields will be coming from and the fields extracted
         """
         model = Images
-        fields = "__all__"
+        fields = ["id","b64_img","team","monster"]
+
+    
+
+    def create(self, validated_data):
+        img_data = validated_data.get("b64_img")
+        img = base64_to_img(img_data)
+        team_data = validated_data.get("team")
+        print(team_data)
+        monster_data = validated_data.get("monster")
+        image, created = Images.objects.update_or_create(
+            image=img, team=team_data, monster=monster_data)
+        return image
