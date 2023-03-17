067413f54d5c15787195704330c772b5a404ed17
vigneshmohan2002 <85409344+vigneshmohan2002@users.noreply.github.com>
2023-03-17 01:34:12 +0000

    fixing rate limiting

--- src/trashmain/settings.py
+++ src/trashmain/settings.py
@@ -159,7 +159,7 @@ REST_FRAMEWORK = {
         "rest_framework.throttling.AnonRateThrottle",
         "rest_framework.throttling.UserRateThrottle",
     ],
-    "DEFAULT_THROTTLE_RATES": {"anon": "15/second", "user": "15/second"},
+    "DEFAULT_THROTTLE_RATES": {"anon": "20/second", "user": "20/second"},
 }
 
 SIMPLE_JWT = {
