a0749b3db667b7383c304ba8a5ef24d8f634f008
vigneshmohan2002 <85409344+vigneshmohan2002@users.noreply.github.com>
2023-03-17 01:21:56 +0000

    removed TMIDSerializer

--- src/trashmonsters/serializer.py
+++ src/trashmonsters/serializer.py
@@ -12,15 +12,3 @@ class TMSerializer(serializers.ModelSerializer):
         """
         model = TrashMonsters
         fields = "__all__"
-
-
-class TMIDSerializer(serializers.ModelSerializer):
-    """ 
-    Used to get specific attributes from the database in JSON format
-    """
-    class Meta:
-        """ 
-        Specifies which model the fields will be coming from and the fields extracted
-        """
-        model = TrashMonsters
-        fields = ["TM_ID"]
