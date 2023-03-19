aec11bef72053dd30c38087f9d0952e272fc27b5
Malik Besta <malik770@hotmail.co.uk>
2023-03-19 15:07:03 +0000

    Update tests.py
    Included a test to ensure that if the same user tries to submit an image twice in the same day, they get a 429 response code

--- src/trashimages/tests.py
+++ src/trashimages/tests.py
@@ -7,19 +7,16 @@ from rest_framework.test import (
     APITestCase,
     APIClient,
     force_authenticate,
-    APIRequestFactory,
 )
 from django.core.files import File
 from rest_framework.parsers import MultiPartParser, FormParser
 
 import io
 
-from PIL import Image
-
 
 class ImageSubmissionViewsetTest(APITestCase):
     def setUp(self):
-        # Create two test users (one player, one gamekeeper)
+        # Create three test users (two players, one gamekeeper)
         self.user_gk = User.objects.create(
             username="example_gamekeeper",
             first_name="test",
@@ -42,6 +39,19 @@ class ImageSubmissionViewsetTest(APITestCase):
             user=self.user_player, team=Team.objects.get_or_create(id=1, name="Red")[0]
         )
 
+        # Test player specifically for API throttling tests
+        self.user_limit = User.objects.create(
+            username="example_player2",
+            first_name="test",
+            last_name="player",
+            email="test_player2@example.com",
+            password="secure_password",
+        )
+
+        self.player_limit = Player.objects.create(
+            user=self.user_limit, team=Team.objects.get_or_create(id=1, name="Red")[0]
+        )
+
         self.trashmonster_one = TrashMonsters.objects.create(Longitude=11, Latitude=2)
 
         # Example Base64 value
@@ -50,26 +60,6 @@ class ImageSubmissionViewsetTest(APITestCase):
         # Initialising the APIs
         self.client = APIClient()
         self.url = "/api"
-
-        def generate_photo_file():
-            """
-            Creates a dummy image that can be used for testing
-            :returns file: Image for testing
-            """
-            file = io.BytesIO()
-            image = Image.new("RGBA", size=(100, 100), color=(155, 0, 0))
-            image.save(file, "png")
-            file.name = "test.png"
-            file.seek(0)
-            return file
-
-        self.image = generate_photo_file()
-
-    def tearDown(self):
-        """
-        Deletes the image created by the test
-        """
-        self.image.__del__()
 
     def test_player_submit_image(self):
         """
@@ -86,6 +76,7 @@ class ImageSubmissionViewsetTest(APITestCase):
             {"b64_img": submission, "monster_id":1},
             format="multipart",
         )
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_gamekeeper_deny_from_submit(self):
@@ -104,6 +95,34 @@ class ImageSubmissionViewsetTest(APITestCase):
             format="multipart",
         )
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_api_limiting(self):
+        """
+        Ensure that players cannot access this endpoint more than one time per day
+        :assertions: Expecting a 201 response code is returned for the first response
+                     Expecting a 429 response code is returned for the second response
+        """
+        parser_classes = (MultiPartParser, FormParser)
+        submission = self.b64_val
+
+        # Getting dummy responses
+        self.client.force_authenticate(self.user_limit)
+        response1 = self.client.post(
+            self.url + "/images/submit-image/",
+            {"b64_img": submission, "monster_id":1},
+            format="multipart",
+        )
+
+        response2 = self.client.post(
+            self.url + "/images/submit-image/",
+            {"b64_img": submission, "monster_id":1},
+            format="multipart",
+        )
+        
+
+        # Asserting that a user can make one post and no more after that 
+        self.assertEqual(response1.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response2.status_code, status.HTTP_429_TOO_MANY_REQUESTS)
 
     def test_gamekeeper_list_image(self):
         """
