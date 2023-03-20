b201e02a643e4820b10546d7d80529dbf001fb8c
vigneshmohan2002 <85409344+vigneshmohan2002@users.noreply.github.com>
2023-03-20 16:24:32 +0000

    setting up email stuff

--- src/trashmain/settings.py
+++ src/trashmain/settings.py
@@ -11,8 +11,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 from datetime import timedelta
+
+# To access environment variables
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-iol&6n@z)0ukoox0bxbn5!s9q5e@icd-s^61c!(8b-il@_ojjd"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -168,5 +173,10 @@ SIMPLE_JWT = {
 }
 
 
-# Allows for the email to be displayed in the console
+# Email settings
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
