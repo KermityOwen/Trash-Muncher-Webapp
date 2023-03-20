074f142aa713260f91566511f0684f6157a3b362
vigneshmohan2002 <85409344+vigneshmohan2002@users.noreply.github.com>
2023-03-20 16:51:32 +0000

    changed email host user

--- src/trashmain/settings.py
+++ src/trashmain/settings.py
@@ -176,7 +176,7 @@ SIMPLE_JWT = {
 # Email settings
 EMAIL_USE_TLS = True
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_USER = 'noreply@trashmunchers.co.uk'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = os.environ.get('EMAIL_PORT')
 
